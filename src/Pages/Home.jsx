132197cfa2a9829a22e5904fbdb86a4a9bd599ac
tanjimtaosif <itsmahtab007@gmail.com>
2025-12-22 22:21:54 +0600

    Add Home component to structure the main page with Banner, FeaturedSection, and ContactUs

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+import FeaturedSection from '../Components/FeaturedSection'
+import ContactUs from '../Components/ContactUs'
+
+const Home = () => {
+  return (
+    <div>
+     <Banner></Banner>
+     <FeaturedSection></FeaturedSection>
+     <ContactUs></ContactUs>
+    </div>
+  )
+}
+
+export default Home
